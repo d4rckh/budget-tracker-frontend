6e1d2401cdaa4919d371d56c88946d3c7c25ebc6
d4rckh <35298550+d4rckh@users.noreply.github.com>
2024-08-29 13:22:35 +0300

    rename

--- components/users/VerifyEmailAlert.tsx
+++ components/users/VerifyEmailAlert.tsx
@@ -24,21 +24,6 @@ export default function VerifyEmailAlert() {
                                                                                         variant={"outline"}
                                                                                         onClick={sendEmailButton}>Press
       here to send email</Button>
-      <p className={"text-gray-400"}>Have a code?
-        <Input
-          onChange={(e) => setVerificationToken(e.target.value)}
-          className={"ml-2 max-w-[350px] inline"}
-          placeholder={"Place it here"}/>
-        <Button className={"ml-1"} variant={"outline"} onClick={() =>
-          verifyEmail(verificationToken)
-            .then((result) => toast({
-              title: result.data ? "Verified email!" : "Wrong verification token supplied"
-            }))
-        }
-        >
-          Verify
-        </Button>
-      </p>
     </AlertDescription>
   </Alert>
 }
